7ce3d751dbf4955dfe304b88af72395c22d836da
william <wmejiamiraw@gmail.com>
2026-01-28 15:01:10 -0600

    Agregar archivos de proyecto.

---
+++ TiendaOnlineAPI/Controllers/ProductosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TiendaOnlineAPI.Models;
+
+namespace TiendaOnlineAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductosController : ControllerBase
+    {
+        private readonly TiendaContext _context;
+
+        public ProductosController(TiendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Producto>>> GetProductos()
+        {
+            return await _context.Productos.Include(p => p.Categoria).ToListAsync();
+        }
+    }
+}
